27a168856e371ae19b9dfd00777463cd7bc1db04
vishnu <109865969+Vishnu2925@users.noreply.github.com>
2022-11-02 17:25:48 +0530

    Add files via upload

---
+++ Foodie_palace.php
@@ -0,0 +1,96 @@
+<html>
+    <head>
+        <title>Foodie Palace</title>
+        <link rel="stylesheet" type="text/css" href="food.css">
+    </head>
+    <body>
+
+        <h1>Foodie Palace</h1>
+        <nav>
+            <ul>
+                <a href="#"><li>Home</li></a>
+                <a href="#"><li>Order</li></a>
+                <a href="#"><li>Near</li></a>
+                <a href="#"><li>About</li></a>
+            </ul>
+        </nav>
+
+        <section class="items">
+            <div class="item">
+                <img src="pizza.jpg">
+                <h4>Pizza veg</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Burger.jpg">
+                <h4>Burger</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="veg-Burger.jpg">
+                <h4>Burger veg</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Biryani.jpg">
+                <h4>Biryani</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Chapathi.jpg">
+                <h4>Chapathi</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Coffiee.jpg">
+                <h4>Coffie</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Chai.jpg">
+                <h4>Chai</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Kabab.jpg">
+                <h4>Kabab</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Leg-Piece.jpg">
+                <h4>Leg-Piece</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Pulka.jpg">
+                <h4>Pulka</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Samosa.jpg">
+                <h4>Samosa</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Parota.jpg">
+                <h4>Parota</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Pav Bhaji.jpg">
+                <h4>Pav Bhaji</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Wings.jpg">
+                <h4>Wings</h4>
+                <button>Order</button>
+            </div>
+            <div class="item">
+                <img src="Chicken 65.jpg">
+                <h4>Chicken 65</h4>
+                <button>Order</button>
+            </div>
+        </section>
+    </body>
+</html>
